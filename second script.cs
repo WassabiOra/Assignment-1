63930a6d97f2d65b5121e5e9574c88ac5329a175
WassabiOra <158175418+WassabiOra@users.noreply.github.com>
2024-02-10 21:41:47 -0500

    Add files via upload

---
+++ second script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class secondscript : MonoBehaviour
+{
+    string var1 = "My name is";
+    string var2 = "Wasid Chowdhury";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(var1 + " " + var2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
